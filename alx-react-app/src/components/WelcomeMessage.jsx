c605560569faf7cdfdb6e16a5ea463353f393768
mohammeddesoky <ms2425033@gmail.com>
2025-09-07 20:25:12 +0300

    task2

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div style ={{ color: 'blue' }}>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
