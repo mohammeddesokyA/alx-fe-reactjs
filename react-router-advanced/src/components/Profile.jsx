c282a5ecf779cd0ca23592c94858e3f9bdf269e0
mohammeddesokyA <ms2425033@gmail.com>
2025-10-20 20:56:24 +0300

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,21 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "../components/ProfileDetails";
+import ProfileSettings from "../components/ProfileSettings";
 
 export default function Profile() {
   return (
     <div>
       <h1>Profile Page</h1>
+
       <nav>
         <Link to="details">Profile Details</Link> |{" "}
         <Link to="settings">Profile Settings</Link>
       </nav>
-      <Outlet />
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
